58fbd9f531e93ae0b7d2a1b21ed89b72ce96763f
Hunter Pittman <hunterpittman604@gmail.com>
2022-10-04 20:37:08 -0700

    Updated webserver_lib added dependecy and vegan tokio testing

--- RustyEDR/src/main.rs
+++ RustyEDR/src/main.rs
@@ -1,13 +1,31 @@
+// Modules
 mod webserver_lib;
 
 use std::fs::File;
 use std::io::prelude::*;
-use webserver_lib::incident_alert;
+use webserver_lib::*;
+use futures::prelude::*;
 
 
-fn main() {
-    let auth = "debug";
-    let payload = "My name jeff";
 
-    incident_alert(auth, payload);
+#[tokio::main]
+async fn main() {
+    let auth = "deug";
+    let incident_info = PartialIncident {
+        name: "bruh_name".to_string(),
+        user: "bruh_user".to_string(),
+        processid: 1190,
+        remoteip: "bruh_remoteip".to_string(),
+        cmdrun: "bruh_cmdrun".to_string()
+    };
+
+    let payload = FullIncident {
+        hostname: "bruh_hostname".to_string(),
+        incident: incident_info
+    };
+
+    //let blocking_task = tokio::task::spawn_blocking(|| {
+        //incident_alert(auth, payload);
+    //});
+    incident_alert(auth, payload).await;
 }
--- RustyEDR/src/webserver_lib.rs
+++ RustyEDR/src/webserver_lib.rs
@@ -1,40 +1,69 @@
 use reqwest::header::{CONTENT_TYPE, HeaderMap, HeaderValue};
-
+use reqwest::Client;
+use futures::prelude::*;
+use serde::{Serialize, Deserialize};
 
 
 const WEB_URL: &str = "test.com";
 
+#[derive(Serialize)]
+pub struct PartialIncident {
+    pub name: String,
+    pub user: String,
+    pub processid: i16,
+    pub remoteip: String,
+    pub cmdrun: String
+}
+
+#[derive(Serialize)]
+pub struct FullIncident {
+    pub hostname: String,
+    pub incident: PartialIncident
+}
+
 
 // POST incident alert to webserver
-pub fn incident_alert(auth: &str, payload: &'static str) -> Result<(), String> {
-
-    let full_url = WEB_URL.to_string() + "Blah Blah boring";
-
-    // Debug Section
-    if auth == "debug" {
-        let output = debug_data();
-        return output;
-    }
-
+pub async fn incident_alert(auth: &str, payload: FullIncident) -> String {
+    //env_logger::init();
+    let endpoint = "BlahBlah/blah";
+    let full_url = WEB_URL.to_string() + endpoint; 
 
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 
 
     let client = reqwest::Client::new();
-    let res = client.post(full_url)
-    .headers(headers)
-    .body(payload)
-    .send();
-    Ok(())
+    let res = client
+        .post(full_url)
+        .headers(headers)
+        .json(&payload)
+        .send()
+        .await;
+
+    match res.unwrap().status() {
+        reqwest::StatusCode::OK => {
+            return "Request delivered".to_string();
+        },
+        _ => {
+            panic!("Request not delivered");
+        }
+    }
 }
+
 
 // GET enviroment IPs from webserver
-pub fn get_ips() -> Result<(), String> {
+pub async fn get_ips() {
 
-
-    Ok(())
 }
+
+
+
+
+
+
+
+
+
 
 
 // GET configs from webserver
@@ -42,16 +71,4 @@ pub fn get_configs() -> Result<(), String> {
 
 
     Ok(())
-}
-
-
-
-
-// Debug function
-fn debug_data() -> Result<(), String> {
-
-    println!("{}", "Bruh moment");
-
-    Ok(())
-
 }
